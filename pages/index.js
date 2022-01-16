438c4476d5c689af2b5c1e39c6d7d40dd84c00e5
Shoaib Salim <shoaib100aib@gmail.com>
2022-01-16 23:21:58 +0530

    fourth commit

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,18 @@
-import Head from 'next/head'
-import Sidebar from '../components/Sidebar';
-import Feed from '../components/Feed';
-import Login from '../components/Login';
-import Modal from '../components/Modal';
-import Widgets from '../components/Widgets';
+import Head from "next/head";
+import Sidebar from "../components/Sidebar";
+import Feed from "../components/Feed";
+import Login from "../components/Login";
+import Modal from "../components/Modal";
+import Widgets from "../components/Widgets";
 import { modalState } from "../atoms/modalAtom";
 import { useRecoilState } from "recoil";
 import { getProviders, getSession, useSession } from "next-auth/react";
 
+export default function Home({ trendingResults, followResults, providers }) {
+  const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useRecoilState(modalState);
 
-export default function Home({trendingResults,followResults,providers}) {
-    const { data: session } = useSession()
-      const [isOpen, setIsOpen] = useRecoilState(modalState);
-
-
-      if (!session) return <Login providers={providers} />;
-
+  if (!session) return <Login providers={providers} />;
 
   return (
     <div className="">
@@ -24,17 +21,16 @@ export default function Home({trendingResults,followResults,providers}) {
         <link rel="icon" href="https://rb.gy/ogau5a" />
       </Head>
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
-    <Sidebar/>
-    <Feed/>
-    <Widgets trendingResults={trendingResults}
+        <Sidebar />
+        <Feed />
+        <Widgets
+          trendingResults={trendingResults}
           followResults={followResults}
         />
-{isOpen && <Modal/>} 
-    
-    </main>
-    
+        {isOpen && <Modal />}
+      </main>
     </div>
-  )
+  );
 }
 
 // 👇 get dynamic props
@@ -47,7 +43,7 @@ export async function getServerSideProps(context) {
   );
   const providers = await getProviders();
   const session = await getSession(context);
-// 👆 so that we have the session before user reaches the page!!
+  // 👆 so that we have the session before user reaches the page!!
   return {
     props: {
       trendingResults,
@@ -57,3 +53,5 @@ export async function getServerSideProps(context) {
     },
   };
 }
+
+//some random changes for git
